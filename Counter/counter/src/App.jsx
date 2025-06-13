b6397f34ea1f822431431fe62d37dcf5e0c062f4
Govind Prajapati <prajapatigovind2210@gmail.com>
2025-06-13 10:37:08 +0530

    Counter App

---
+++ Counter/counter/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    if (count < 0) {
+      alert("Count cannot be Negative");
+      setCount(0)
+    }
+  },[count])
+  return (
+    <>
+      <h1>Counter</h1>
+      <div className="card">
+        <button onClick={() => setCount(count + 1)} >+</button>
+        <h2>{count}</h2>
+        <button onClick={() => setCount(count - 1)} >-</button>
+      </div>
+    </>
+  )
+}
+
+export default App
